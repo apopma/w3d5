8a3fff726cbf278876bc996f415816db4c6d3a1a
apopma <adam.popma@gmail.com>
2015-07-10 11:59:11 -0700

    SQLObject finds objects by ID

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -6,13 +6,15 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # how can self.table_name be parameterized? is this a risk?
-    db_cols = DBConnection.execute2(<<-SQL)
+    return @db_cols if @db_cols
+
+    @db_cols ||= DBConnection.execute2(<<-SQL)
       SELECT *
       FROM #{self.table_name}
       LIMIT 0
     SQL
 
-    db_cols.flatten.map(&:to_sym)
+    @db_cols.flatten!.map!(&:to_sym)
   end
 
   def self.finalize!
@@ -34,15 +36,30 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    raw_db_results = DBConnection.execute(<<-SQL)
+      SELECT #{self.table_name}.*
+      FROM #{self.table_name}
+    SQL
+
+    self.parse_all(raw_db_results)
   end
 
   def self.parse_all(results)
-    # ...
+    # map, not each - we want the new array here
+    results.map { |result| self.new(result) }
   end
 
   def self.find(id)
-    # ...
+    results = DBConnection.execute(<<-SQL, id)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    # ::parse_all will return either empty array, or 1-size array with this id
+    # [].first => nil, anything_else.first => the found object
+    self.parse_all(results).first
   end
 
   def initialize(params = {})
