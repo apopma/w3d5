c54a76a4f51fefed05f7f5e18be4f411f87c2590
apopma <adam.popma@gmail.com>
2015-07-10 13:56:59 -0700

    write Searchable module and Searchable#where

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    whereline = params.map { |key, _| "#{key} = ?" }
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{whereline.join(" AND ")}
+    SQL
+
+    results.map { |result| self.new(result) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
