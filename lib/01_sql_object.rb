2e2d01ad14abfcd5baf17197ec47593f12557f79
apopma <adam.popma@gmail.com>
2015-07-10 11:29:30 -0700

    pass specs thru phase 1d

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,18 +5,32 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    # how can self.table_name be parameterized? is this a risk?
+    db_cols = DBConnection.execute2(<<-SQL)
+      SELECT *
+      FROM #{self.table_name}
+      LIMIT 0
+    SQL
+
+    db_cols.flatten.map(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |column_name|
+      define_method("#{column_name}") { attributes[column_name] }
+
+      define_method("#{column_name}=") do |val|
+        attributes[column_name] = val
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || "#{self}".tableize # still messes up 'human' though
   end
 
   def self.all
@@ -32,15 +46,21 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, attr_val|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+
+      self.send("#{attr_name}=", attr_val)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    attributes.values
   end
 
   def insert
